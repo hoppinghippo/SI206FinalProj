c6ee72a82fe79b214e0c42f4462f3a6878089c88
darametz <dnmetz@umich.edu>
2015-03-25 09:55:17 -0400

    added form

--- survey.php
+++ survey.php
@@ -25,6 +25,25 @@
             $filename = basename(__FILE__);
             include("header.php"); 
         ?>
+    <form action="" method="post">
+        <fieldset>
+            <legend>Contact Us</legend>
+            <label>Name:<input type="name" name="name" placeholder="Jim Harbaugh" required/></label><br />
+            <label>Email:<input type="email" name="email" placeholder="jimharbaugh@umich.edu" required/></label><br />
+            
+            <legend>Year:</legend>
+            <input type="radio" name="year" value="Freshman"/>Freshman<br />
+            <input type="radio" name="year" value="Sophomore"/>Sophomore<br />
+            <input type="radio" name="year" value="Junior"/>Junior<br />
+            <input type="radio" name="year" value="Senior"/>Senior<br />
+
+            <label>Major:<input type="major" name="major" placeholder="Major"></label><br />
+            <label>Questions/Comments/Concerns:</label><br />
+                <textarea rows="4" cols="50"></textarea><br />
+
+            <button type="submit">SUBMIT</button>
+            </fieldset>
+        </form>
 
  
 
