ebbc2505abcf9a04518fda75b7d006c239ded3f6
asuldan <asuldan@umich.edu>
2015-04-14 16:08:06 -0400

    last modified in footer

--- footer.php
+++ footer.php
@@ -1,5 +1,13 @@
 <footer>
     <p>reference: <a href="http://en.wikipedia.org/wiki/Jim_Harbaugh">http://en.wikipedia.org/wiki/Jim_Harbaugh</a></p>
     <p>&copy; Brandon Deere | Don Weidner | Abby Suldan | Dara Metz</p>
-    <p>Last updated: April 15th 2015</p>
+    <p>
+    	<?php
+		
+if (file_exists($filename)){
+echo "Last modified: " . date ("F d Y H:i:s.", filemtime($filename)); 
+ } 
+
+		?>
+</p>
 </footer>
