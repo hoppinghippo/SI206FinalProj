2f50fdbd6a4ed610d902152004a3c4af8a2cbf53
asuldan <asuldan@umich.edu>
2015-04-14 15:27:20 -0400

    styling and alt text for images

--- photo.php
+++ photo.php
@@ -31,52 +31,52 @@
             
               <ul class="thumbnails">
                 <li class="span3">
-                        <a class="thumbnail col-sm-3" rel="lightbox[group]" href="photos/img1.jpg"><img class="group1" src="photos/tn1.jpg" alt=""/></a>
+                        <a class="thumbnail col-sm-3" rel="lightbox[group]" href="photos/img1.jpg"><img class="group1" src="photos/tn1.jpg" alt="49ers coach"/></a>
                     </li>
                    <li class="span3">
-                        <a class="thumbnail col-sm-3" rel="lightbox[group]" href="photos/img2.jpg"><img class="group1" src="photos/tn2.jpg" /></a>
+                        <a class="thumbnail col-sm-3" rel="lightbox[group]" href="photos/img2.jpg"><img class="group1" src="photos/tn2.jpg" alt="Jim as Michigan coach"/></a>
                     </li>
                     <li class="span3">
-                        <a class="thumbnail col-sm-3" rel="lightbox[group]" href="photos/img3.jpg"><img class="group1" src="photos/tn3.jpg"/></a>
+                        <a class="thumbnail col-sm-3" rel="lightbox[group]" href="photos/img3.jpg"><img class="group1" src="photos/tn3.jpg" alt="The Harbaugh family"/></a>
                     </li>
                     <li class="span3">
-                        <a class="thumbnail col-sm-3" rel="lightbox[group]" href="photos/img4.jpg"><img class="group1" src="photos/tn4.jpg" /></a>
+                        <a class="thumbnail col-sm-3" rel="lightbox[group]" href="photos/img4.jpg"><img class="group1" src="photos/tn4.jpg" alt="Jim in college"/></a>
                     </li> 
                     <li class="span3">
-                        <a class="thumbnail col-sm-3" rel="lightbox[group]" href="photos/img5.jpg"><img class="group1" src="photos/tn5.jpg" /></a>
+                        <a class="thumbnail col-sm-3" rel="lightbox[group]" href="photos/img5.jpg"><img class="group1" src="photos/tn5.jpg" alt="Jim as Ravens QB"/></a>
                     </li> 
                     <li class="span3">
-                        <a class="thumbnail col-sm-3" rel="lightbox[group]" href="photos/img6.jpg"><img class="group1" src="photos/tn6.jpg" /></a>
+                        <a class="thumbnail col-sm-3" rel="lightbox[group]" href="photos/img6.jpg"><img class="group1" src="photos/tn6.jpg" alt="Jim as Chargers QB"/></a>
                     </li> 
                     <li class="span3">
-                        <a class="thumbnail col-sm-3" rel="lightbox[group]" href="photos/img7.jpg"><img class="group1" src="photos/tn7.jpg" /></a>
+                        <a class="thumbnail col-sm-3" rel="lightbox[group]" href="photos/img7.jpg"><img class="group1" src="photos/tn7.jpg" alt="Jim wins as Stanford coach"/></a>
                     </li> 
                     <li class="span3">
-                        <a class="thumbnail col-sm-3" rel="lightbox[group]" href="photos/img8.jpg"><img class="group1" src="photos/tn8.jpg" /></a>
+                        <a class="thumbnail col-sm-3" rel="lightbox[group]" href="photos/img8.jpg"><img class="group1" src="photos/tn8.jpg" alt="Jim as Stanford coach"/></a>
                     </li>
                      <li class="span3">
-                        <a class="thumbnail col-sm-3" rel="lightbox[group]" href="photos/img9.jpg"><img class="group1" src="photos/tn9.jpg" /></a>
+                        <a class="thumbnail col-sm-3" rel="lightbox[group]" href="photos/img9.jpg"><img class="group1" src="photos/tn9.jpg" alt="Jim chosen as 49ers coach"/></a>
                     </li>
                      <li class="span3">
-                        <a class="thumbnail col-sm-3" rel="lightbox[group]" href="photos/img10.jpg"><img class="group1" src="photos/tn10.jpg" /></a>
+                        <a class="thumbnail col-sm-3" rel="lightbox[group]" href="photos/img10.jpg"><img class="group1" src="photos/tn10.jpg" alt="Jim playing golf"/></a>
                     </li>
                      <li class="span3">
-                        <a class="thumbnail col-sm-3" rel="lightbox[group]" href="photos/img12.jpg"><img class="group1" src="photos/tn12.jpg" /></a>
+                        <a class="thumbnail col-sm-3" rel="lightbox[group]" href="photos/img12.jpg"><img class="group1" src="photos/tn12.jpg" alt="Harbaugh family Superbowl"/></a>
                     </li>
                      <li class="span3">
-                        <a class="thumbnail col-sm-3" rel="lightbox[group]" href="photos/img13.jpg"><img class="group1" src="photos/tn13.jpg" /></a>
+                        <a class="thumbnail col-sm-3" rel="lightbox[group]" href="photos/img13.jpg"><img class="group1" src="photos/tn13.jpg" alt="Jim as Michigan student athlete"/></a>
                     </li>
                      <li class="span3">
-                        <a class="thumbnail col-sm-3" rel="lightbox[group]" href="photos/img14.jpg"><img class="group1" src="photos/tn14.jpg" /></a>
+                        <a class="thumbnail col-sm-3" rel="lightbox[group]" href="photos/img14.jpg"><img class="group1" src="photos/tn14.jpg" alt="Jim and Goofy"/></a>
                     </li>
                      <li class="span3">
-                        <a class="thumbnail col-sm-3" rel="lightbox[group]" href="photos/img15.jpg"><img class="group1" src="photos/tn15.jpg" /></a>
+                        <a class="thumbnail col-sm-3" rel="lightbox[group]" href="photos/img15.jpg"><img class="group1" src="photos/tn15.jpg" alt="Jim at Michigan press conference"/></a>
                     </li>
                      <li class="span3">
-                        <a class="thumbnail col-sm-3" rel="lightbox[group]" href="photos/img16.jpg"><img class="group1" src="photos/tn16.jpg" /></a>
+                        <a class="thumbnail col-sm-3" rel="lightbox[group]" href="photos/img16.jpg"><img class="group1" src="photos/tn16.jpg" alt="Jim and John Harbaugh"/></a>
                     </li>
                      <li class="span3">
-                        <a class="thumbnail col-sm-3" rel="lightbox[group]" href="photos/img17.jpg"><img class="group1" src="photos/tn17.jpg" /></a>
+                        <a class="thumbnail col-sm-3" rel="lightbox[group]" href="photos/img17.jpg"><img class="group1" src="photos/tn17.jpg" alt="Jim as Michigan QB"/></a>
                     </li>
             </ul><!--end thumbnails -->
 
